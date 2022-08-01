66582f58190da42fa3f761e17d431890943c4f12
Michael An <huayu.michael@gmail.com>
2022-07-31 23:29:24 -0700

    updated problem 5

--- problems/06-closure-sandwiches.js
+++ problems/06-closure-sandwiches.js
@@ -23,19 +23,6 @@ const sandwichMaker = () => {
 	return (addFood = food => (order = `${order} and ${food}`));
 };
 
-let sandwich = sandwichMaker(); // => returns a function
-sandwich('spinach'); // => "One sandwich with tomato and spinach"
-console.log("🚀 ~ sandwich('spinach')", sandwich('spinach'));
-sandwich('jelly'); // => "One sandwich with tomato and spinach and jelly"
-console.log("🚀 ~ sandwich('jelly')", sandwich('jelly'));
-sandwich('bread'); // => "One sandwich with tomato and spinach and jelly and bread"
-console.log("🚀 ~ sandwich('bread')", sandwich('bread'));
-
-// Another Sandwich:
-let sandwich2 = sandwichMaker(); // => returns a function
-sandwich2('pb'); // => "One sandwich with tomato and pb"
-console.log("🚀 ~ sandwich2('pb')", sandwich2('pb'));
-
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
